cff07a2095502a37d54c701480b8236656d63925
Kelly Elias <kelly.elias@gmail.com>
2018-05-10 21:10:49 -0600

    Cleanup

--- Pillage/FileScanner.cs
+++ Pillage/FileScanner.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Pillage
 {
@@ -10,9 +9,13 @@ namespace Pillage
         public static List<string> GetFiles(string parentFolder, string filePattern,
             Dictionary<string, string> ignoredExtensions, bool searchSubfolders)
         {
-            var allFiles = Directory.GetFiles(parentFolder, filePattern, searchSubfolders? SearchOption.AllDirectories:SearchOption.TopDirectoryOnly);
+            var allFiles = Directory.GetFiles(parentFolder, filePattern,
+                searchSubfolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
 
-            var files =  (from f in allFiles let ext = Path.GetExtension(f).Replace(".", "").ToLower() where !ignoredExtensions.ContainsKey(ext) select f).ToList();
+            var files = (from f in allFiles
+                let ext = Path.GetExtension(f).Replace(".", "").ToLower()
+                where !ignoredExtensions.ContainsKey(ext)
+                select f).ToList();
 
             return files;
         }
